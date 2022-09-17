95a4be10d040b03f2ab4596abf59ec344917d7d1
eungwang <scm323@naver.com>
2022-09-17 20:19:39 +0900

    파일명 변경

--- seleniumAutomation/naver_login_automation.py
+++ seleniumAutomation/naver_login_automation.py
@@ -2,14 +2,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
 # 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
 
 import time
-import pyautogui
 import pyperclip
 import util
 
@@ -18,7 +17,7 @@ load_dotenv()
 
 # 브라우저 꺼짐 방지
 chrome_options = Options()
-chrome_options.add_experimental_option("detach",True)
+chrome_options.add_experimental_option("detach", True)
 
 # 불필요한 에러 메시지 없애기
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -26,10 +25,11 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 # 웹페이지 해당 주소 이동
-driver.implicitly_wait(5) # 웹 페이지가 로딩 될때까지 5초는 기다린다
-driver.maximize_window() # 화면크기 최대화
+driver.implicitly_wait(5)  # 웹 페이지가 로딩 될때까지 5초는 기다린다
+driver.maximize_window()  # 화면크기 최대화
 
-driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com")
+driver.get(
+    "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com")
 
 NAVER_ID = os.environ.get('NAVER_ID')
 NAVER_PASSWORD = os.environ.get('NAVER_PASSWORD')
@@ -37,20 +37,16 @@ NAVER_PASSWORD = os.environ.get('NAVER_PASSWORD')
 id = driver.find_element(By.CSS_SELECTOR, "#id")
 id.click()
 pyperclip.copy(NAVER_ID)
-util.keydownAndPress("command","v")
+util.keydownAndPress("command", "v")
 time.sleep(2)
 
 # 비밀번호 입력창
 pw = driver.find_element(By.CSS_SELECTOR, "#pw")
 pw.click()
 pyperclip.copy(NAVER_PASSWORD)
-util.keydownAndPress("command","v")
+util.keydownAndPress("command", "v")
 time.sleep(2)
 
 # 로그인 버튼
-login_btn = driver.find_element(By.CSS_SELECTOR,"#log\.login")
+login_btn = driver.find_element(By.CSS_SELECTOR, "#log\.login")
 login_btn.click()
-
-
-
-
